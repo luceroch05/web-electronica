a584447666a901eca21fa8e87c3ede8b78f35025
lucero <luc3r0.ch4ngr4.99.com>
2024-06-04 16:51:14 -0500

    agregue salon

--- models/salon.php
+++ models/salon.php
@@ -11,14 +11,29 @@ class Salon{
     public static function find($id){
         global $conexion;
         $stmt=$conexion->prepare("SELECT * FROM salon WHERE id_salon=?");
-        stmt->bind_param("id",$id);
+        stmt->bind_param("i",$id);
         stmt->execute();
         return $stmt->get_result()->fetch_assoc();
     }
 
     public static function create($data){
         global $conexion;
-        $stmt=$conexion->prepare("INSERT INTO salon(id_Sa")
+        $stmt=$conexion->prepare("INSERT INTO salon(nombre_salon VALUES (?)");
+        $stmt->bind_param("s",$data['nombre_salon']);
+        stmt->execute();
+    }
+    public static function update($id, $data) {
+        global $conexion;
+        $stmt = $conexion->prepare("UPDATE salon SET  nombre_salon=? WHERE id_salon = ?");
+        $stmt->bind_param("ssisssssii",$data['nombre_saloin'], $id);
+        $stmt->execute();
+    }
+
+    public static function delete($id) {
+        global $conexion;
+        $stmt = $conexion->prepare("DELETE FROM salon WHERE id_salon = ?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
     }
 
 
